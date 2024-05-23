b8b6dcaa348fda78428d810dfc99dda200e76f34
Thibault <tbmc33@gmail.com>
2024-05-23 18:48:04 +0200

    feat: change default folder

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn main() {
     env_logger::Builder::new().filter_level(LevelFilter::Info).init();
 
     info!("Execute BrainFuck script!");
-    let result = interpret_script_file("brain_fuck_scripts/prime_2.bf");
+    let result = interpret_script_file(format!("{}/prime_2.bf", SCRIPT_FOLDER).as_str());
     match result {
         Ok(_) => {
             info!("Success!");
