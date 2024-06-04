9672817ffe657eb7a09e90c5466650726bcbe9c5
Thibault <tbmc33@gmail.com>
2024-06-04 16:23:45 +0200

    Remove old websocketServer

--- websocketServer/src/app.ts
+++
@@ -1,7 +0,0 @@
-import { server } from './proxy';
-import { runWebSocketServer } from './websocket';
-
-runWebSocketServer();
-
-server.listen(3001);
-console.log('Listening HTTP and WebSocket on port 3001');
--- websocketServer/test/example.test.ts
+++
@@ -1,5 +0,0 @@
-import { test, expect } from 'vitest';
-
-test('Example Test', () => {
-    expect(true).toBe(true);
-});
